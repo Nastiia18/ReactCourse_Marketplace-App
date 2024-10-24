6bf032e23bc2292a37ec2e28a6b2190a1a8e7a31
Nastia Bondar <145715374+Nastiia18@users.noreply.github.com>
2024-10-25 02:57:41 +0300

    Initial routing and HTTP client setup

---
+++ react-routing/src/components/Layout.tsx
@@ -0,0 +1,29 @@
+import { NavLink, Outlet } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/products">Product</NavLink>
+          </li>
+          <li>
+            <NavLink to="/categories">Category</NavLink>
+          </li>
+          <li>
+            <NavLink to="/users">User</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+export default Layout;
---
+++ react-routing/src/pages/products/ProductsPage.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+}
+
+const ProductsPage = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Products Page</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.title} - ${product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductsPage;
